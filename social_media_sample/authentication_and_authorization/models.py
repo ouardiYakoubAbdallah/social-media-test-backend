0737a7a9459c62fdc9b0e2578d966f0dae6f05c5
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 15:54:52 +0100

    feat: add follows relationship

--- social_media_sample/authentication_and_authorization/models.py
+++ social_media_sample/authentication_and_authorization/models.py
@@ -39,10 +39,28 @@ class UserManager(BaseUserManager):
 
         return user
 
+
+class Follow(models.Model):
+    user = models.ForeignKey(to='User', related_name='following', on_delete=models.CASCADE)
+    followed_user = models.ForeignKey(to='User', related_name='followed_by', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+    def __str__(self):
+        return f"{self.user.username} follows {self.followed_user.username}"
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
     email = models.EmailField(validators=[validators.validate_email], unique=True, blank=False)
     registration_date = models.DateTimeField(auto_now_add=True)
+    followers = models.ManyToManyField(
+        to='self',
+        through=Follow,
+        symmetrical=False,
+        blank=True
+    )
 
     is_staff = models.BooleanField(default=False)
 
