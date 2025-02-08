6516a1c999eb03a65758d7929cdc41f790e100dc
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 14:57:03 +0100

    feat: implement register, login and profile views using generics

--- social_media_sample/authentication_and_authorization/views.py
+++ social_media_sample/authentication_and_authorization/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as LoginSerializer
 
-# Create your views here.
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+class UserProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
+
