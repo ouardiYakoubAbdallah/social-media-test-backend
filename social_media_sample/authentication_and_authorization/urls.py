fce032a37e473f5f965a98c65614aa1dae8f020d
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 20:46:36 +0100

    feat: add unfollow user endpoint url

--- social_media_sample/authentication_and_authorization/urls.py
+++ social_media_sample/authentication_and_authorization/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('users', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('profile/<pk>', UserProfileView.as_view(), name='profile'),
-    path('users/follow/<str:username>', FollowView.as_view(), name='follow')
+    path('users/follow/<str:username>', FollowView.as_view(), name='follow'),
+    path('users/unfollow/<str:username>', UnfollowView.as_view(), name='unfollow')
 ]
