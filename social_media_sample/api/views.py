a424e0f9d1df4198679f1be693160307b52c9db0
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 17:18:35 +0100

    feat: implement posts feed view

--- social_media_sample/api/views.py
+++ social_media_sample/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostsFeedView(generics.GenericAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    allowed_methods = ['GET', 'POST']
+
+    def get_queryset(self):
+        followed_users = self.request.user.following.values_list('followed_user', flat=True)
+        return Post.objects.filter(author__in=followed_users).order_by('-timestamp')
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
