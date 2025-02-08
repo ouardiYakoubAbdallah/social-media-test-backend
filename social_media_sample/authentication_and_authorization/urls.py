c78ca6af405f1ba5e45b0170df2eb0c93264edd9
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 15:57:09 +0100

    feat: add follow users endpoint url

--- social_media_sample/authentication_and_authorization/urls.py
+++ social_media_sample/authentication_and_authorization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView as LoginView)
-from .views import RegisterView, UserProfileView
+from .views import *
 
 urlpatterns = [
     path('users', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('profile/<pk>', UserProfileView.as_view(), name='profile'),
+    path('users/follow/<str:username>', FollowView.as_view(), name='follow')
 ]
