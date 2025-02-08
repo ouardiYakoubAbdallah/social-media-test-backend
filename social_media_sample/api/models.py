35dd0734f9ed4270ae105c75700031db57020363
Yakoub Abdallah OUARDI <ouardi.yakoubabdallah@gmail.com>
2025-02-08 17:17:09 +0100

    feat: add Post model

--- social_media_sample/api/models.py
+++ social_media_sample/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField(blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        to='authentication_and_authorization.User',
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.timestamp}"
